2e27d9f0364531c054c2923eaf75c417a073fa55
Chen Zhiyong <756957385@qq.com>
2012-04-19 15:40:43 +0800

    First commit

---
+++ TestOfBowling/Frame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestOfBowling
+{
+    public class Frame
+    {
+        private int score;
+        
+        public int Score
+        {
+            get { return score; }
+        }
+
+        internal void Add(int pins)
+        {
+            score += pins;
+        }
+    }
+}
